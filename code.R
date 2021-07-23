22e65b03e200bfc761a7dcba2958f960fa63df52
Danilo Ferreira de Oliveira <danilo_f_oliveira@hotmail.com>
2021-07-22 22:15:01 -0300

    All updated files and bibliography addition

--- code.R
+++ code.R
@@ -13,7 +13,6 @@ library(dplyr)
 library(caret)
 library(corrplot)
 library(pROC)
-#
 
 
 bank_full <- read.csv('bank-additional-full.csv', sep = ';')
@@ -206,11 +205,7 @@ table(bank_full$y, bank_full$previous) # put 3-7 together?
 
 
 
-
-#### BARPLOTS, HISTOGRAMS, QQPLOT, BOXPLOT
 # Modeling
-
-# fct_lump
 
 
 bank_full_2 <- bank_full %>% mutate(job = as.factor(job), marital = as.factor(marital), 
@@ -247,52 +242,7 @@ roc_glm$auc #0.79
 rm(model,probabilities,predicted.classes,roc_glm)
 
 
-#knn
-set.seed(123, sample.kind = "Rounding")
-pred_F1 <- function(k){
-  fit <- knn3(y ~ ., data = train, k = k)
-  y_hat <- predict(fit, test, type = "class")
-  F_meas(data = y_hat, reference = as.factor(test$y), relevant='yes')
-  
-}
 
-k <- seq(2,10)
-
-F1 <- sapply(k, pred_F1)
-plot(k, F1)
-max(F1)
-k[which.max(F1)]
-
-
-fit <- knn3(y ~ ., data = train, k = k[which.max(F1)])
-y_hat <- predict(fit, test, type = "class")
-F_meas(data = y_hat, reference = as.factor(test$y), relevant='yes')
-
-cm <- confusionMatrix(as.factor(y_hat), as.factor(test$y), positive = 'yes')
-cm
-
-roc_knn <- roc(response=as.ordered(y_hat), predictor=as.ordered(test$y), auc=TRUE)
-roc_knn$auc #0.6351
-
-rm(F1,y_hat,F_meas,roc_knn,k,pred_F1,fit)
-
-
-#rpart
-ctrl <- trainControl(method = "cv",number=5, classProbs = TRUE, summaryFunction = twoClassSummary)
-set.seed(123, sample.kind = "Rounding")
-fit_tree <- train(y ~ ., data = train, method = 'rpart', tuneGrid = data.frame(cp = seq(0, 0.05, 0.002)),
-                  metric="ROC", trControl = ctrl)
-
-fit_tree$results$cp[which.max(fit_tree$results$Accuracy)]
-
-tree_pred <- predict(fit_tree, test)
-cm <- confusionMatrix(as.factor(tree_pred), as.factor(test$y), positive = 'yes')
-cm
-
-roc_rpart <- roc(response=as.ordered(tree_pred), predictor=as.ordered(test$y), auc=TRUE)
-roc_rpart$auc #0.7092
-
-rm(fit_tree,tree_pred,roc_rpart)
 
 
 # Random Forest
@@ -311,7 +261,9 @@ roc_rf$auc #0.7414
 
 rm(rf_pred,roc_rf,fit_rf)
 
-#gbm
+#Generalized Boosted Regression Model
+
+
 set.seed(123, sample.kind = "Rounding")
 fit_gbm <- train(y ~ ., data = train, 
                  method = "gbm", 
@@ -327,184 +279,4 @@ roc_gbm <- roc(response=as.ordered(gbm_pred), predictor=as.ordered(test$y), auc=
 roc_gbm$auc #0.7846
 
 rm(fit_gbm,gbm_pred,roc_gbm)
-
-
-
-#Rborist
-
-library(Rborist)
-control <- trainControl(method="cv", number = 5, p = 0.8)
-grid <- expand.grid(minNode = c(1,5) , predFixed = c(10, 15, 25, 35, 50))
-set.seed(123, sample.kind = "Rounding")
-train_rf <-  train(y ~ ., data= train,
-                   method = "Rborist",
-                   nTree = 50,
-                   trControl = control,
-                   tuneGrid = grid,
-                   nSamp = 5000)
-ggplot(train_rf)
-train_rf$bestTune
-
-set.seed(123, sample.kind = "Rounding")
-fit_rf <- Rborist(x=train %>% select(-y), train$y,
-                  nTree = 1000,
-                  minNode = train_rf$bestTune$minNode,
-                  predFixed = train_rf$bestTune$predFixed)
-
-y_hat_rf <- predict(fit_rf, test%>%select(-y))
-cm <- confusionMatrix(as.factor(y_hat_rf$yPred), as.factor(test$y))
-cm
-
-roc_rborist <- roc(response=as.ordered(y_hat_rf$yPred), predictor=as.ordered(test$y), auc=TRUE)
-roc_rborist$auc #0.818
-
-rm(control,grid,y_hat_rf,roc_rborist,fit_rf,train_rf)
-
-
-
-library(purrr) # for functional programming (map)
-
-ctrl <- trainControl(method = "repeatedcv",
-                     number = 10,
-                     repeats = 5,
-                     summaryFunction = twoClassSummary,
-                     classProbs = TRUE)
-
-orig_fit <- train(y ~ .,
-                  data = train,
-                  method = "gbm",
-                  verbose = FALSE,
-                  metric = "ROC",
-                  trControl = ctrl)
-
-model_weights <- ifelse(train$y == "no",
-                        (1/table(train$y)[1]) * 0.5,
-                        (1/table(train$y)[2]) * 0.5)
-
-# Use the same seed to ensure same cross-validation splits
-
-ctrl$seeds <- orig_fit$control$seeds
-
-# Build weighted model
-
-weighted_fit <- train(y ~ .,
-                      data = train,
-                      method = "gbm",
-                      verbose = FALSE,
-                      weights = model_weights,
-                      metric = "ROC",
-                      trControl = ctrl)
-
-ctrl$sampling <- "down"
-
-down_fit <- train(y ~ .,
-                  data = train,
-                  method = "gbm",
-                  verbose = FALSE,
-                  metric = "ROC",
-                  trControl = ctrl)
-
-ctrl$sampling <- "up"
-
-up_fit <- train(y ~ .,
-                  data = train,
-                  method = "gbm",
-                  verbose = FALSE,
-                  metric = "ROC",
-                  trControl = ctrl)
-
-model_list <- list(original = orig_fit,
-                   weighted = weighted_fit,
-                   down = down_fit,
-                   up = up_fit)
-
-test_roc <- function(model, data) {
-  
-  roc(data$y,
-      predict(model, data, type = "prob")[, "yes"])
-  
-}
-
-model_list_roc <- model_list %>%
-  map(test_roc, data = test)
-
-model_list_roc %>%
-  map(auc)
-
-
-results_list_roc <- list(NA)
-num_mod <- 1
-
-for(the_roc in model_list_roc){
-  
-  results_list_roc[[num_mod]] <- 
-    data_frame(tpr = the_roc$sensitivities,
-               fpr = 1 - the_roc$specificities,
-               model = names(model_list)[num_mod])
-  
-  num_mod <- num_mod + 1
-  
-}
-
-results_df_roc <- bind_rows(results_list_roc)
-
-# Plot ROC curve for all 4 models
-
-custom_col <- c("#000000", "#009E73", "#0072B2", "#D55E00")
-
-ggplot(aes(x = fpr,  y = tpr, group = model), data = results_df_roc) +
-  geom_line(aes(color = model), size = 1) +
-  scale_color_manual(values = custom_col) +
-  geom_abline(intercept = 0, slope = 1, color = "gray", size = 1) +
-  theme_bw(base_size = 18)
-
-rm(model_list,model_list_roc,results_df_roc,results_list_roc,orig_fit,up_fit,down_fit,
-   the_roc,weighted_fit,model_weights,custom_col,num_mod)
-
-library(ROSE)
-
-data.rose <- ROSE(y ~ ., data = train, seed = 1)$data
-
-data_balanced_over <- ovun.sample(y ~ ., data = train, method = "over")$data
-table(data_balanced_over$y)
-
-data_balanced_under <- ovun.sample(y ~ ., data = train, method = "under", seed = 1)$data
-table(data_balanced_under$y)
-
-data_balanced_both <- ovun.sample(y ~ ., data = train, method = "both", seed = 1)$data
-table(data_balanced_both$y)
-
-
-library(rpart)
-
-tree.rose <- rpart(y ~ ., data = data.rose)
-tree.over <- rpart(y ~ ., data = data_balanced_over)
-tree.under <- rpart(y ~ ., data = data_balanced_under)
-tree.both <- rpart(y ~ ., data = data_balanced_both)
-
-pred.tree.rose <- predict(tree.rose, newdata = test)
-pred.tree.over <- predict(tree.over, newdata = test)
-pred.tree.under <- predict(tree.under, newdata = test)
-pred.tree.both <- predict(tree.both, newdata = test)
-
-
-roc.curve(test$y, pred.tree.rose[,2])
-
-roc.curve(test$y, pred.tree.over[,2])
-
-#AUC Undersampling
-roc.curve(test$y, pred.tree.under[,2])
-
-#AUC Both
-roc.curve(test$y, pred.tree.both[,2])
-
-
-ROSE.holdout <- ROSE.eval(y ~ ., data = train, learner = rpart, method.assess = "holdout",
-                          extr.pred = function(obj)obj[,2], seed = 1)
-ROSE.holdout
-
-
-rm(pred.tree.both,pred.tree.over,pred.tree.rose,pred.tree.under,data_balanced_both,data.rose,
-   data_balanced_over,data_balanced_under,tree.both,tree.over,tree.rose,tree.under, ROSE.holdout)
-
 
